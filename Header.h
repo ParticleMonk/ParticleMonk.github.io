f8e635150ff4fceb7e43e4c4d01306f957d1e0c9
ParticleMonk <dhjpr7@mail.umkc.edu>
2019-06-26 14:08:40 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -68,7 +68,7 @@ private:
 public:
 	void add_employee(employee employee);//Adds a new employee to PQ in the right priority location
 	void circulate_book(book book, date start_date);//adds a new book to the list book_in_circ and gives it to the top employee in PQ and removes that employee to has_books - sets start for employee - sets wait for employee
-	void pass_on(book book, string employee, date current_date);//updates retainer(employee.start-book.current_date) for current holding employee - sets prioirty for ncurrent holding employee - update book with current date - removes next employee from PQ, adds this employee to has_book and sets that emlpoyees wait(current_date- book.start_date) - returns the previous holding employee to PQ - increment use on book
+	void pass_on(string book, string newEmployee, date current_date);//updates retainer(employee.start-book.current_date) for current holding employee - sets prioirty for ncurrent holding employee - update book with current date - removes next employee from PQ, adds this employee to has_book and sets that emlpoyees wait(current_date- book.start_date) - returns the previous holding employee to PQ - increment use on book
 	void archive_book(book book)// checks to see if book needs to be archives and if so move the book from list book_in_circ and put it in list archives
 
 
