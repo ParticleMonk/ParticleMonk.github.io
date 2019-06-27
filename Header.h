9d057ce08f0b7211f49f958bcc425899a0aecafd
ParticleMonk <dhjpr7@mail.umkc.edu>
2019-06-27 08:55:36 -0500

    All changes needed to implement Library class UPDATED

--- Header.h
+++ Header.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <queue>
@@ -23,6 +24,9 @@ public:
 	int get_wait();
 	date get_start_date();
 	string get_book();
+	string get_name();
+
+
 
 
 private:
@@ -45,13 +49,14 @@ private:
 };
 class book {
 public:
-book(string name, date start_date);
+	book(string name, date start_date);
 	void use();//increments times_used
 	void update_current(date newdate);//updates current date
 	date get_current_date();//Returns the current date
 	date get_start_date(); //Returns the start date
 	string get_name();
 	int get_usage();//returns usage
+
 private:
 	string name;
 	date start_date, current_date;
@@ -61,7 +66,7 @@ private:
 
 class Library {
 private:
-	queue<employee> PQ;
+	vector<employee> PQ;
 	list<employee> has_books;
 	list<book> book_in_circ;
 	list<book> archives;
@@ -72,7 +77,7 @@ public:
 	void add_employee(employee employee);//Adds a new employee to PQ in the right priority location
 	void circulate_book(string bookIncoming, date start_date);//adds a new book to the list book_in_circ and gives it to the top employee in PQ and removes that employee to has_books - sets start for employee - sets wait for employee
 	void pass_on(string bookPass, string newEmployee, date current_date);//updates retainer(employee.start-book.current_date) for current holding employee - sets prioirty for ncurrent holding employee - update book with current date - removes next employee from PQ, adds this employee to has_book and sets that emlpoyees wait(current_date- book.start_date) - returns the previous holding employee to PQ - increment use on book
-	bool archive_book(book book);// checks to see if book needs to be archives and if so move the book from list book_in_circ and put it in list archives
+	void archive_book(book bookChecked);// checks to see if book needs to be archives and if so move the book from list book_in_circ and put it in list archives
 	void remove_employee(employee employeeBye); // Removes employee from the has_books list. 
 
 
