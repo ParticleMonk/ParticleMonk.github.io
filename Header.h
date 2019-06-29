dbd0a9f5f33bbf0625f61e886a46c824f46e75d5
ParticleMonk <dhjpr7@mail.umkc.edu>
2019-06-29 12:41:48 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -8,24 +8,30 @@
 
 using namespace std;
 
-
 class employee {
 
 public:
-	employee();
-	string compare();// Takes another employee and compares it to the current employee and returns "greater", "less" or "equal"
-	void set_retainer(int retainer);
-	void set_wait(int newwait);
-	void set_priority(int newpriority);
-	void set_start(date startDate);
-	void set_book(string newBook);
-	int get_priority();
-	int get_retainer();
-	int get_wait();
-	date get_start_date();
-	string get_book();
-	string get_name();
-	bool has_read(string bookTitle); //Should search read vector and return true if book name is in it. 
+	employee(string employeeName) { name = employeeName, wait_time = 0, retain_time = 0, priority = 0; };
+	//string compare();// Takes another employee and compares it to the current employee and returns "greater", "less" or "equal"	void set_retainer(int retainer) {retain_time = retainer ; };
+	//unsure how to implement this yet, need to know how we plan on passing in employees to compare
+	// my guess is that we need to pass in the employees name, pull their priority and then retun the name with the higher priority
+	// vector<string> compare(list<employee> employeeList) {
+		//iterate through the list
+		// sort based on higest priority
+		// return sorted vector, could also be a sorted list
+	void set_wait(int newwait) { wait_time = newwait; };
+	void set_retainer(int retainer) {retain_time = retainer;}
+	void set_priority(int newpriority) { priority = newpriority; };
+	void set_start(date startDate) { start = startDate; };
+	void set_book(string newBook) { book = newBook; };
+	int get_priority() { return priority; };
+	int get_retainer() { return retain_time; };
+	int get_wait() { return wait_time; };
+	date get_start_date() { return start; };
+	string get_book() { return book; };
+	string get_name() { return name; };
+	bool has_read(string bookTitle); //Should search read vector and return true if book name is in it.
+	void add_book_to_history(string bookTitle);
 
 
 
@@ -33,7 +39,7 @@ public:
 private:
 	string name;
 	date start;
-	int wait_time, ratain_time, priority;
+	int wait_time, retain_time, priority;
 	string book;
 	vector<string> read;
 };
@@ -51,7 +57,7 @@ private:
 };
 class book {
 public:
-	book(string name, date start_date);
+	book(string name, date dateIncoming);
 	void use();//increments times_used
 	void update_current(date newdate);//updates current date
 	date get_current_date();//Returns the current date
@@ -63,24 +69,4 @@ private:
 	string name;
 	date start_date, current_date;
 	int times_used;
-};
-
-
-class Library {
-private:
-	vector<employee> PQ;
-	list<employee> has_books;
-	list<book> book_in_circ;
-	list<book> archives;
-	int num_employees;
-
-
-public:
-	void add_employee(employee employee);//Adds a new employee to PQ in the right priority location
-	void circulate_book(string bookIncoming, date start_date);//adds a new book to the list book_in_circ and gives it to the top employee in PQ and removes that employee to has_books - sets start for employee - sets wait for employee
-	void pass_on(string bookPass, string newEmployee, date current_date);//updates retainer(employee.start-book.current_date) for current holding employee - sets prioirty for ncurrent holding employee - update book with current date - removes next employee from PQ, adds this employee to has_book and sets that emlpoyees wait(current_date- book.start_date) - returns the previous holding employee to PQ - increment use on book
-	void archive_book(book bookChecked);// checks to see if book needs to be archives and if so move the book from list book_in_circ and put it in list archives
-	void remove_employee(employee employeeBye); // Removes employee from the has_books list. 
-
-
 };
