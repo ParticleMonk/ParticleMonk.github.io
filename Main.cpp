afb90d76b2119e146a0a8ec52774a501ed3a4623
matth1073 <46977315+matth1073@users.noreply.github.com>
2019-06-29 13:26:47 -0500

    Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Library.h"
+#include "Employee.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	Library library;
+	library.addEmployee("Adam");
+	library.addEmployee("Sam");
+	library.addEmployee("Ann");
+
+	library.addBook("Chemistry");
+	library.addBook("Software Engineering");
+
+	library.circulateBook("Chemistry", Date(2015, 3, 1));
+	library.circulateBook("Software Engineering", Date(2015, 4, 1));
+
+	library.passOn("Chemistry", Date(2015, 3, 5));
+	library.passOn("Chemistry", Date(2015, 3, 7));
+	library.passOn("Chemistry", Date(2015, 3, 15));
+
+	library.passOn("Software Engineering", Date(2015, 4, 5));
+	library.passOn("Software Engineering", Date(2015, 4, 10));
+	library.passOn("Software Engineering", Date(2015, 4, 15));
+
+	return 0;
+}
